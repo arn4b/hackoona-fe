8d3e8bd7dc746b557ee58a2c369024690cd3b3fa
Arnab Chatterjee <arnabc1701@gmail.com>
2023-04-08 01:31:35 +0530

    inital commit

---
+++ src/components/SingleCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function SingleCard() {
+  return (
+    <>
+      <div className="bg-white shadow-md rounded-md overflow-hidden h-140 w-full relative">
+        <div className="px-4 py-2">
+          <h2 className="text-lg font-medium text-gray-800">Issue: fix padding in navbar</h2>
+          <p className="text-sm text-gray-600">Subtitlyere</p>
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ src/pages/issues.tsx
@@ -0,0 +1,13 @@
+import Layout from '@/components/Layout'
+import SingleCard from '@/components/SingleCard'
+import React from 'react'
+
+export default function Issues() {
+    return (
+        <Layout>
+            <div className='w-9/12 mx-auto my-10'>
+                <SingleCard />
+            </div>
+        </Layout>
+    )
+}
